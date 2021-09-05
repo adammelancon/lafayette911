586cebf0cb37aea4028cf0c4ae498fdf96ace9f5
Adam Melancon <adammelancon@gmail.com>
2021-09-05 15:38:18 -0500

    cleanup and functions

--- lafayette911.py
+++ lafayette911.py
@@ -1,10 +1,15 @@
-import requests
+import requests, re, json, unicodedata
 from bs4 import BeautifulSoup
-import re
-import json
+from datetime import datetime
 
 incidents = []
 incidents_clean = []
+
+def get_time():
+    global incidents_clean
+    now = datetime.now()
+    date_time = now.strftime("%m/%d/%Y, %H:%M:%S")
+    return "Last Ran - " + str(date_time)
 
 def soup_me():
     global incidents
@@ -13,11 +18,11 @@ def soup_me():
     r = requests.post(url, data = myobj)
     soup = BeautifulSoup(r.text, 'html.parser')
     for tr in soup.find_all(target="_new"):
-        incidents.append(tr)    
+        incidents.append(tr)
 
 def check_incidents():
     if not incidents:
-        ni = "No incidents at the moment."
+        ni = "No incidents at " + get_time()
         print(ni)
         with open('lafayette911.json', 'w') as outfile:
             json.dump(ni, outfile)
@@ -27,19 +32,23 @@ def check_incidents():
 def process_incidents():
     global incidents_clean
     for row in incidents:
-        print("=" * 50)
         row = row.text
         row = re.compile(r'(<[^>]+>)|(")|({)|(})').sub('', row)
         row = row.strip('/}')
         row = row.replace("\\", "")
-        print(row)
-        print("=" * 50)
+        row = unicodedata.normalize("NFKD",row)
         incidents_clean.append(row)
-        
+
+    incidents_clean.append(get_time())
+    
     with open('lafayette911.json', 'w') as outfile:
         json.dump(incidents_clean, outfile)
 
+def display_incidents():
+    print(incidents_clean)
+
 soup_me()
 check_incidents()
+display_incidents()
 
     
