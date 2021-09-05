2aa2022833930699af0be16bdad3026d7e25b088
Adam Melancon <adammelancon@gmail.com>
2021-09-05 15:00:03 -0500

    added a json file to dump incidents to

--- lafayette911.py
+++ lafayette911.py
@@ -1,23 +1,31 @@
 import requests
 from bs4 import BeautifulSoup
 import re
+import json
 
 incidents = []
+incidents_clean = []
 
-url = 'https://apps.lafayettela.gov/L911/Service2.svc/getTrafficIncidents'
-myobj = {'name': 'e'}
-r = requests.post(url, data = myobj)
-soup = BeautifulSoup(r.text, 'html.parser')
-# print(soup.prettify())
+def soup_me():
+    global incidents
+    url = 'https://apps.lafayettela.gov/L911/Service2.svc/getTrafficIncidents'
+    myobj = {'name': 'e'}
+    r = requests.post(url, data = myobj)
+    soup = BeautifulSoup(r.text, 'html.parser')
+    for tr in soup.find_all(target="_new"):
+        incidents.append(tr)    
 
-rows = soup.find_all(target="_new")
+def check_incidents():
+    if not incidents:
+        ni = "No incidents at the moment."
+        print(ni)
+        with open('lafayette911.json', 'w') as outfile:
+            json.dump(ni, outfile)
+    else:
+        process_incidents()
 
-for tr in soup.find_all(target="_new"):
-    incidents.append(tr)    
-
-if not incidents:
-    print("No incidents at the moment.")
-else:
+def process_incidents():
+    global incidents_clean
     for row in incidents:
         print("=" * 50)
         row = row.text
@@ -26,3 +34,12 @@ else:
         row = row.replace("\\", "")
         print(row)
         print("=" * 50)
+        incidents_clean.append(row)
+        
+    with open('lafayette911.json', 'w') as outfile:
+        json.dump(incidents_clean, outfile)
+
+soup_me()
+check_incidents()
+
+    
